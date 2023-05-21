01f81fbc7bf305c8e0fd7fb5876b075ef1a10ac4
DerTimm81 <timm@dertimm.de>
2023-05-21 23:23:34 +0200

    defined an ini-file to handle configuration options

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-# just a test for now.
+from wl_connection import WeatherLink
+from pprint import pprint
+import configparser
+
+config = configparser.ConfigParser()
+
+# Fill out the 'config_sample.ini' with meaningful content and remove the '_sample'-phrase in the filename, so you get your own 'config.ini'
+# From this point on, you will see your own stuff.
+config.read('config.ini')
+
+API_KEY = config['API']['ApiKey']
+API_SECRET = config['API']['ApiSecret']
+
+
+# Create a WeatherLink object using API keys
+wl = WeatherLink(API_KEY, API_SECRET)
+
+stations = wl.stations().json()
+station_id = stations['stations'][0]['station_id']
+
+current_data = wl.current(station_id).json()
+
+
+inside_temp = current_data['sensors'][0]['data'][0]['temp_in']
+inside_temp_celsius = (inside_temp - 32) * 5/9
+print('Inside Temperature is at: ' + str(inside_temp_celsius) + ' °C')
+
+outside_temp = current_data['sensors'][1]['data'][0]['temp']
+outside_temp_celsius = (outside_temp - 32) * 5/9
+print('Outside Temperature is at: ' + str(outside_temp_celsius) + ' °C')
---
+++ wl_connection.py
@@ -0,0 +1,50 @@
+########################################################################################################################
+# 
+########################################################################################################################
+
+import time
+import hmac
+import hashlib
+import requests
+import collections
+
+
+class WeatherLink:
+
+    API_URL_V2 = 'https://api.weatherlink.com/v2/'
+
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret.encode('utf-8')
+        self.timestamp = int(time.time())
+
+    def signature(self, params={}):
+        self.timestamp = int(time.time())
+
+        params['api-key'] = self.api_key
+        params['t'] = self.timestamp
+
+        params = collections.OrderedDict(sorted(params.items()))
+
+        payload = ''
+        for key in params:
+            payload = payload + key + str(params[key])
+
+        return hmac.new(self.api_secret, payload.encode('utf-8'), hashlib.sha256).hexdigest()
+
+    def stations(self, station_ids=[]):
+        stations_str = ','.join(station_ids)
+        query_params = {
+            'api-key': self.api_key,
+            'api-signature': self.signature({'station-ids': stations_str} if station_ids else {}),
+            't': str(self.timestamp)
+        }
+        return requests.get(self.API_URL_V2 + 'stations/' + stations_str, params=query_params)
+
+    def current(self, station_id):
+        query_params = {
+            'api-key': self.api_key,
+            'api-signature': self.signature({'station-id': station_id}),
+            't': str(self.timestamp)
+        }
+        return requests.get(self.API_URL_V2 + 'current/' + str(station_id), params=query_params)
