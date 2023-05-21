fce59b5550d7a9e231c319ef07b25eb9ec6e250b
DerTimm81 <timm@dertimm.de>
2023-05-21 23:52:33 +0200

    using a package to connect to weatherlink locally

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from wl_connection import WeatherLink
 from pprint import pprint
 import configparser
 
+import logging
+import time
+
+import weatherlink_live_local as wlll
+
+logging.basicConfig(level=logging.ERROR)
+
 config = configparser.ConfigParser()
 
 # Fill out the 'config_sample.ini' with meaningful content and remove the '_sample'-phrase in the filename, so you get your own 'config.ini'
@@ -11,20 +18,44 @@ config.read('config.ini')
 API_KEY = config['API']['ApiKey']
 API_SECRET = config['API']['ApiSecret']
 
+if config['API']['ConnectToWeatherLinkServer'] == "True":
 
-# Create a WeatherLink object using API keys
-wl = WeatherLink(API_KEY, API_SECRET)
+    print("Trying to connect to your WeatherLink account (via Internet) ...")
 
-stations = wl.stations().json()
-station_id = stations['stations'][0]['station_id']
+    # Create a WeatherLink object using API keys
+    wl = WeatherLink(API_KEY, API_SECRET)
 
-current_data = wl.current(station_id).json()
+    stations = wl.stations().json()
+    station_id = stations['stations'][0]['station_id']
+
+    current_data = wl.current(station_id).json()
 
 
-inside_temp = current_data['sensors'][0]['data'][0]['temp_in']
-inside_temp_celsius = (inside_temp - 32) * 5/9
-print('Inside Temperature is at: ' + str(inside_temp_celsius) + ' °C')
+    inside_temp = current_data['sensors'][0]['data'][0]['temp_in']
+    inside_temp_celsius = (inside_temp - 32) * 5/9
+    print(f"Inside Temperature is at: {inside_temp_celsius:.2f} °C")
 
-outside_temp = current_data['sensors'][1]['data'][0]['temp']
-outside_temp_celsius = (outside_temp - 32) * 5/9
-print('Outside Temperature is at: ' + str(outside_temp_celsius) + ' °C')
+    outside_temp = current_data['sensors'][1]['data'][0]['temp']
+    outside_temp_celsius = (outside_temp - 32) * 5/9
+    print(f"Outside Temperature is at: {outside_temp_celsius:.2f} °C")
+
+if config['API']['ConnectToLocalWeatherLink'] == "True":
+    print("Trying to connect to WeatherLink in your network (just using local wifi)...")
+
+    devices = wlll.discover()
+    # print(devices)
+
+    # select first device, get IP address
+    ip_first_device = devices[0].ip_addresses[0]
+
+    # specify units
+    wlll.set_units(
+        temperature=wlll.units.TemperatureUnit.CELSIUS,
+        pressure=wlll.units.PressureUnit.HECTOPASCAL,
+        rain=wlll.units.RainUnit.MILLIMETER,
+        wind_speed=wlll.units.WindSpeedUnit.METER_PER_SECOND,
+    )
+
+    conditions = wlll.get_conditions(ip_first_device)
+    print(f"Inside temperature:  {conditions.inside.temp:.2f} °C")
+    print(f"Outside temperature: {conditions.integrated_sensor_suites[0].temp:.2f} °C")
